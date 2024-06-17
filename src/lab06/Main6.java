169fa35d1d85ecc576e70efe6a5a5e9e61889b79
Lucia Liu <112028165+lliu285@users.noreply.github.com>
2024-06-17 12:27:38 -0700

    did main

--- src/lab06/Main6.java
+++ src/lab06/Main6.java
@@ -1,9 +1,57 @@
 package lab06;
 
+//import lab05.Dollar;
+//import lab05.HashTable;
+
 public class Main6 
 {
 	public static void main(String[] args) 
 	{
+		Dollar[] dollars = new Dollar[20];
+		dollars[0] = new Dollar(57.12);
+		dollars[1] = new Dollar(23.44);
+		dollars[2] = new Dollar(87.43);
+		dollars[3] = new Dollar(68.99);
+		dollars[4] = new Dollar(111.22);
+		dollars[5] = new Dollar(44.55);
+		dollars[6] = new Dollar(77.77); 
+		dollars[7] = new Dollar(18.36);
+		dollars[8] = new Dollar(543.21); 
+		dollars[9] = new Dollar(20.21);
+		dollars[10] = new Dollar(345.67);
+		dollars[11] = new Dollar(36.18);
+		dollars[12] = new Dollar(48.48);
+		dollars[13] = new Dollar(101.00);
+		dollars[14] = new Dollar(11.00);
+		dollars[15] = new Dollar(21.00);
+		dollars[16] = new Dollar(51.00);
+		dollars[17] = new Dollar(1.00);
+		dollars[18] = new Dollar(251.00);
+		dollars[19] = new Dollar(151.00);
+		
+		MinHeap heap = new MinHeap();
+		
+		for (int i = 0; i < 10; i++) {
+			heap.insert(dollars[i]);
+		}
+		
+		// traversals
+		System.out.println("Heap after 10 insertions----------------------------------------");
+		heap.breadthFirstTraversal(); 
+		heap.inOrderTraversal(); 
+		heap.preOrderTraversal();
+		heap.postOrderTraversal();
+		
+		for (int i = 10; i < dollars.length; i++) {
+			heap.insert(dollars[i]);
+		}
+		
+		// traversals
+		System.out.println("\n\nHeap after 20 insertions----------------------------------------");
+		heap.breadthFirstTraversal(); 
+		heap.inOrderTraversal(); 
+		heap.preOrderTraversal();
+		heap.postOrderTraversal();
 		
 	}
 }
--- src/lab06/MinHeap.java
+++ src/lab06/MinHeap.java
@@ -1,19 +1,34 @@
 package lab06;
 
+import java.io.PrintStream;
+
 public class MinHeap
 {
 	//private HeapNode[] heap;
 	private HeapNode root;
-	private int size;
+	private int height;
 	
 	public MinHeap()
 	{
 		root = null;
+		height = 0;
 	}
 	
 	private void percolateUp()
 	{
 		
+		
+		/* note: this is for max heap
+		while (nodeIndex > 0) {
+		      parentIndex = (nodeIndex - 1) / 2
+		      if (heapArray[nodeIndex] <= heapArray[parentIndex])
+		         return
+		      else {
+		         swap heapArray[nodeIndex] and heapArray[parentIndex]
+		         nodeIndex = parentIndex
+		      }
+	   	}
+		 */
 	}
 	
 	private void percolateDown()
@@ -21,9 +36,11 @@ public class MinHeap
 		
 	}
 	
-	private void swap(int pos1, int pos2)
+	private void swap(HeapNode node1, HeapNode node2)
 	{
-		
+		BSTNode temp = node1;
+		node1.setParent(node2);
+		node2.setLeft(temp); // setRight??
 	}
 	
 	public void insert(Dollar dollar) 
@@ -39,5 +56,222 @@ public class MinHeap
 	public void delete(Dollar dollar)
 	{
 		
+	}
+	
+	/*
+	 * Pre:
+	 * title - String of specified traversal type.
+	 * 
+	 * Post:
+	 * Prints a header for the specified traversal type in console and output file.
+	 */
+	private void printTitle(String title) 
+	{
+		System.out.println("\n" + title + ": ");
+	}
+	
+	/*
+	 * Pre:
+	 * node - BSTNode that will be printed.
+	 * 
+	 * Post:
+	 * Prints data of given node in console and output file.
+	 */
+	private void printNode(BSTNode node) 
+	{
+		System.out.print(node.getDollar() + " ");
+	}
+
+	/*
+	 * Pre:
+	 * ps - File-based PrintStream object for the output file.
+	 * 
+	 * Post:
+	 * Prints the in-order traversal of the binary search tree.
+	 */
+	public void inOrderTraversal() 
+	{
+		printTitle("In-order traversal");
+		inOrderTraversalHelper(root);
+	}
+
+	/*
+	 * Pre:
+	 * node - A reference to a node in the binary search tree or null.
+	 * 
+	 * Post:
+	 * Prints the in-order traversal of the binary search tree rooted at the specified node.
+	 */
+	private void inOrderTraversalHelper(BSTNode node) 
+	{
+		/*
+		Pseudocode:
+		If the node is not null:
+        	Call inOrderTraversalHelper(node's left child)
+            printNode the node
+            Call inOrderTraversalHelper(node's right child)
+		 */
+		
+		if (node != null) {
+			inOrderTraversalHelper(node.getLeft());
+			printNode(node);
+			inOrderTraversalHelper(node.getRight());
+		}
+	}
+
+	/*
+	 * Pre:
+	 * ps - File-based PrintStream object for the output file.
+	 * 
+	 * Post:
+	 * Prints the pre-order traversal of the binary search tree.
+	 */
+	public void preOrderTraversal() 
+	{
+		printTitle("Pre-order traversal");
+		preOrderTraversalHelper(root);
+	}
+
+	/*
+	 * Pre:
+	 * node - A reference to a node in the binary search tree or null.
+	 * 
+	 * Post:
+	 * Prints the pre-order traversal of the binary search tree rooted at the specified node.
+	 */
+	private void preOrderTraversalHelper(BSTNode node) 
+	{
+		/*
+		Pseudocode:
+		If the node is not null:
+        	printNode the node
+            Call preOrderTraversalHelper(node's left child)
+            Call preOrderTraversalHelper(node's right child)
+		 */
+		
+		if (node != null) {
+			printNode(node);
+			preOrderTraversalHelper(node.getLeft());
+			preOrderTraversalHelper(node.getRight());
+		}
+	}
+
+	/*
+	 * Pre:
+	 * ps - File-based PrintStream object for the output file.
+	 * 
+	 * Post:
+	 * Prints the post-order traversal of the binary search tree.
+	 */
+	public void postOrderTraversal() 
+	{
+		printTitle("Post-order traversal");
+		postOrderTraversalHelper(root);
+	}
+
+	/*
+	 * Pre:
+	 * node - A reference to a node in the binary search tree or null.
+	 * 
+	 * Post:
+	 * Prints the post-order traversal of the binary search tree rooted at the specified node.
+	 */
+	private void postOrderTraversalHelper(BSTNode node) {
+		/*
+		Pseudocode:
+		If the node is not null:
+        	Call postOrderTraversalHelper(node's left child)
+            Call postOrderTraversalHelper(node's right child)
+			printNode the node
+		 */
+		
+		if (node != null) {
+			postOrderTraversalHelper(node.getLeft());
+			postOrderTraversalHelper(node.getRight());
+			printNode(node);
+		}
+	}
+
+	/*
+	 * Pre:
+	 * ps - File-based PrintStream object for the output file.
+	 * 
+	 * Post:
+	 * Prints the breadth-first traversal of the binary search tree.
+	 */
+	public void breadthFirstTraversal() {
+		/*
+		Pseudocode:
+        PrintTitle "Breadth-first traversal"
+        Call getHeight(root) to set the height of the tree
+        From level 1 to i(the height of the tree):
+            Call breadthFirstTraversalHelper(root,i)
+		 */
+		
+		printTitle("Breadth-first traversal");
+		int height = getHeight(root);
+		
+		for (int i = 1; i <= height; i++)
+			breadthFirstTraversalHelper(root, i);
+	}
+	
+	/*
+	 * Post:
+	 * Returns the height of the specified node.
+	 */
+	private int getHeight(BSTNode node) {
+		/*
+		Pseudocode:
+		If the node is null:
+			Return 0
+		Call getHeight(node's left child) to set the height of the left subtree
+        Call getHeight(node's right child) to set the height of the right subtree
+		if left subtree's height > right subtree's height: 
+            return left subtree's height + 1 
+		otherwise:
+			return right subtree's height + 1
+		 */
+		
+		if (node == null)
+			return 0;
+		else {
+			int lheight = getHeight(node.getLeft());
+			int rheight = getHeight(node.getRight());
+
+			if (lheight > rheight)
+				return (lheight + 1);
+			else
+				return (rheight + 1);
+		}
+	}
+
+	/*
+	 * Pre:
+	 * node - A reference to a node in the binary search tree or null.
+	 * level - Positive integer indicating the level in the binary search tree.
+	 * 
+	 * Post:
+	 * Prints the nodes at the specified level in the binary search tree rooted at the specified node.
+	 */
+	private void breadthFirstTraversalHelper(BSTNode node, int level) {
+		/*
+		Pseudocode:
+		If the node is null:
+        	Return; (exit out of method)
+		If the level == 1:
+			printNode the node
+		If the level > 1:
+			Call breadthFirstTraversalHelper(node's left child,level - 1)
+			Call breadthFirstTraversalHelper(node's right child,level - 1)
+		 */
+		
+		if (node == null)
+			return;
+		if (level == 1)
+			printNode(node);
+		else if (level > 1) {
+			breadthFirstTraversalHelper(node.getLeft(), level - 1);
+			breadthFirstTraversalHelper(node.getRight(), level - 1);
+		}
 	}
 }
