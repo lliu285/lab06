664b21f6187b9fa54d248a5575ad286eaf17a6cc
Lucia Liu <112028165+lliu285@users.noreply.github.com>
2024-06-17 11:15:20 -0700

    commit1

--- src/lab06/MinHeap.java
+++ src/lab06/MinHeap.java
@@ -2,24 +2,39 @@ package lab06;
 
 public class MinHeap
 {
+	//private HeapNode[] heap;
+	private HeapNode root;
+	private int size;
 	
 	public MinHeap()
+	{
+		root = null;
+	}
+	
+	private void percolateUp()
 	{
 		
 	}
 	
-	
-	public boolean search(Dollar dollar) 
+	private void percolateDown()
 	{
-		return true;
+		
 	}
 	
+	private void swap(int pos1, int pos2)
+	{
+		
+	}
 	
 	public void insert(Dollar dollar) 
 	{
 		
 	}
 	
+	public boolean search(Dollar dollar) 
+	{
+		return true;
+	}
 	
 	public void delete(Dollar dollar)
 	{
